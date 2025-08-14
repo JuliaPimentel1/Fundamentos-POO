af4871955d4511c3416a453cea65bea21d393727
Juliapimentel1 <JuliaPimentel1>
2025-08-14 14:58:24 -0300

    AppTarefa

---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        public int TarefaId { get; set; } // Id é a chave primária
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+    }
+}
