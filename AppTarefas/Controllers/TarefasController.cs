ec65ad0d14c5eb46770a65224138ddac93b9d428
Juliapimentel1 <JuliaPimentel1>
2025-08-21 11:52:28 -0300

    App

--- AppTarefas/Controllers/TarefasController.cs
+++ AppTarefas/Controllers/TarefasController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Runtime.CompilerServices;
+using AppTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AppTarefas.Controllers
 {
     public class TarefasController : Controller
     {
+        //Lista em memória(grava as informações apenas quando a aplicação está rodando)
+        private static List<Tarefa> _tarefas = new List<Tarefa>();
+        private static int _proximoId = 1;
+
+        // GET: Tarefas
         public IActionResult Index()
+        {
+            return View(_tarefas);
+        }
+
+        // GET : Tarefas/Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                tarefa.TarefaId = _proximoId++;
+                _tarefas.Add(tarefa);
+                return RedirectToAction("Index");
+            }
+            return View(tarefa);
+        }
+
+        //GET: Tarefas/Edit/1
+        public IActionResult Edit(int id)
+        {
+            // var tarefa = _tarefas[id]; // Trabalhando com Lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        //POST: Tarefas/Edit/1
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Tarefa tarefaAtualizada)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+
+            tarefa.Titulo = tarefaAtualizada.Titulo;
+            tarefa.Descricao = tarefaAtualizada.Descricao;
+            tarefa.Concluida = tarefaAtualizada.Concluida;
+
+            return RedirectToAction("Index");
+        }
+        //GET: Details/Edit/1
+        public IActionResult Details(int id)
+        {
+            // var tarefa = _tarefas[id]; // Trabalhando com Lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        //GET: Delete/1
+        public IActionResult Delete(int id)
+        {
+            // var tarefa = _tarefas[id]; // Trabalhando com Lista
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            return View(tarefa);
+        }
+
+        // POST: Tarefas/Delete/1
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa != null)
+            {
+                _tarefas.Remove(tarefa);
+            }
+            return RedirectToAction("Index");
+        }
     }
+
 }
+
+
